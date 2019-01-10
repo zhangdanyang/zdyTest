07da014704c8a443510d51fef427117404a0e41e
zhangdanyang <1084749690@qq.com>
2019-01-10 23:18:01 +0800

    测试提交

---
+++ demo-git/src/main/java/com/lay/demo_git/App.java
@@ -0,0 +1,13 @@
+package com.lay.demo_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
